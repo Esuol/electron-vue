fbd6c28a33e3efbe44aef1decd8527cf9832f85d
weibin <1292393108@qq.com>
2019-04-04 16:04:31 +0800

    update

--- out/electron-vue-demo-darwin-x64/electron-vue-demo.app/Contents/Resources/app/src/views/frame.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-     <a class="protocol" href="electron-api-demos://open"><h3>electron-api-demos://open</h3></a>
-      <p>点击上面的链接返回到 Electron API 示例.</p>
-  </div>
-</template>
